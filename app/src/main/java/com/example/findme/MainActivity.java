86160f7f79847e6359f68870f7b0a158b6b152fe
LiamUpstoneSmith <liamjake2002@hotmail.co.uk>
2024-04-21 00:54:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/findme/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.findme;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void launchSettings(View v){
+
+        //launch new activity
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    public void launchContacts(View v){
+
+        //launch new activity
+        Intent i = new Intent(this, ContactsActivity.class);
+        startActivity(i);
+    }
+}
